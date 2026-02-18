b5e1b54331afd4a3cee7f34de9e79e35ab58a6e5
jellebaele <jellebaele@hotmail.com>
2026-02-18 19:17:19 +0100

    Add basic security to express server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,22 @@
+import cors from 'cors';
 import express, { type Application } from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 const app: Application = express();
 
-// app.use(cors())
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56 // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+});
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
+app.disable('x-powered-by');
 
 export default app;
