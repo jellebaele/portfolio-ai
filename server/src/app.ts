f680fa36af350026b6029531d7394939e080e407
jellebaele <jellebaele@hotmail.com>
2026-02-18 19:10:56 +0100

    Adjust nodemon + basic setup express

---
+++ server/src/app.ts
@@ -0,0 +1,8 @@
+import express, { type Application } from 'express';
+
+const app: Application = express();
+
+// app.use(cors())
+app.use(express.json());
+
+export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello world!');
+import app from './app';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
