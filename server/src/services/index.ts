50a15fc78014acc5b7d39fdaf9112e77c01cb038
jellebaele <jellebaele@hotmail.com>
2026-03-02 18:35:05 +0100

    Add Dockerfiles

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,7 +1,7 @@
 import { llmProvider } from '@/llm';
-import ResumeService from './ResumeService';
 import VectorService from './VectorService';
 import IngestService from './ingestService';
+import ResumeService from './resumeService';
 
 const vectorService = new VectorService(llmProvider);
 export const resumeService = new ResumeService(llmProvider, vectorService);
