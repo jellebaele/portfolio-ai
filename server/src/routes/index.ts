23088b4388ff6d386008f74474d7fe76b67882d9
jellebaele <jellebaele@hotmail.com>
2026-03-04 14:32:24 +0100

    Add health router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import chatRouter from './chatRouter';
+import healthRouter from './healthRouter';
 
 const apiRouter = Router();
 
 apiRouter.use('/chat', chatRouter);
+apiRouter.use('/health', healthRouter);
 
 export default apiRouter;
